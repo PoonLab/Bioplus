2380df18ba1fd5789318490725e698be8abdb53b
Art Poon <artpoon@gmail.com>
2024-01-21 16:19:24 -0500

    working on date guessing

--- add_dates.py
+++ add_dates.py
@@ -1,12 +1,41 @@
 import argparse
 from Bio import SeqIO
 from csv import DictReader
+from datetime import date
+import re
+
 
 description = """\
 Use metadata to relabel sequences in the associated FASTA file.
 Optionally, use the dates to down-sample the sequences to a target 
 size so that dates are evenly distributed.
 """
+
+def lubridate(string, delim=',|-|/| '):
+    """
+    Guess the date format of a string, starting with ISO format.
+    :param string:  str, serialization of a calendar date in unknown format
+    :param delim:  str, a regular expression comprising all potential characters
+                   delimiting date fields, separated by "|"
+    :return:  datetime.date object
+    """
+    try:
+        # first try ISO format
+        return date.fromisoformat(string)
+    except ValueError:
+        items = re.split(delim, string)
+        if len(items) == 1:
+            try:
+                return int(string)  # year?
+            except ValueError:
+                return None  # failed
+        elif len(items) == 2:
+            # try some month-year combos
+            
+        elif len(items) == 3:
+
+    return None
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=description)
