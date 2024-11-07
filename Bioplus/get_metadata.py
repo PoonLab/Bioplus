906a2a648580835dd1937154ec84d5eb65845bfe
ArtPoon <artpoon@gmail.com>
2024-11-07 16:02:43 -0500

    wrote install script

---
+++ Bioplus/get_metadata.py
@@ -0,0 +1,164 @@
+from Bio import Entrez, SeqIO, Phylo
+import sys
+import time
+import argparse
+import re
+from csv import DictWriter
+
+description = """\
+Retrieve metadata (e.g., sample collection dates) associated with sequences 
+in the input file, based on their respective NCBI Genbank accession numbers.
+"""
+
+pat1 = re.compile("[A-Z]{1,3}[0-9]{5,8}\\.[0-9]|[NXAYW][CGTWZMRP]_[A-Z0-9]+\\.[0-9]")
+pat2 = re.compile("[A-Z]{1,3}[0-9]{5,8}\\.[0-9]|[NXAYW][CGTWZMRP]_[A-Z0-9]+")
+
+
+def find_accn(label, versioned=True):
+    """
+    Extract Genbank accession number from an input string.
+    :param label:  str, string to process
+    :param versioned:  bool, if True then accession number must have ".[0-9]" suffix
+    :return:  first matching substring, or None if no matches
+    """
+    if versioned:
+        hits = pat1.findall(label)
+    else:
+        hits = pat2.findall(label)
+    if len(hits) == 0:
+        return None
+    return hits[0]
+
+
+def get_metadata(accn, fields, db='nucleotide'):
+    """
+    Retrieve source metadata associated with sequence for a given accession
+    number.
+    :param accn:  str, Genbank accession number.  Multiple numbers may be
+                  submitted as a comma-delimited string.
+    :param fields:  tuple, field names to extract from source qualifier, e.g.,
+                    ("country", "collection_date").
+    :param db:  str, Genbank database to query
+    :return:  dict, values associated with fields
+    """
+    handle = Entrez.efetch(db=db, rettype='gb', retmode='text', id=accn,
+                           seq_start=1, seq_stop=1)
+    output = SeqIO.parse(handle, format='genbank')
+    for record in output:
+        source = record.features[0]
+        values = [source.qualifiers.get(f, [''])[0] for f in fields]
+        yield dict(zip(fields, values))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "infile", type=argparse.FileType('r'),
+        help="Path to file containing sequences or tree. Labels MUST contain Genbank "
+             "accession numbers."
+    )
+    parser.add_argument(
+        "--email", type=str, required=True,
+        help="E-mail address for NCBI transactions."
+    )
+    parser.add_argument(
+        "-f", "--format", type=str, default='fasta',
+        help="Format of input sequence/tree file; must be supported by Bio.SeqIO or "
+             "Phylo.  Defaults to 'fasta' (use 'newick' for trees).")
+    parser.add_argument(
+        "--db", type=str, default="nucleotide",
+        help="Database to query, default 'nucleotide'."
+    )
+    parser.add_argument(
+        "--delay", type=float, default=3.0,
+        help="Number of seconds to pause between queries (default 3)."
+    )
+    parser.add_argument(
+        "--batch", type=int, default=50,
+        help="Number of records to retrieve per request (default 50)."
+    )
+    parser.add_argument(
+        "-o", "--outfile", type=argparse.FileType('w'), default=sys.stdout,
+        help="option, path to write CSV output.  If --relabel, then "
+             "outputs a new sequence or tree file.  Default is stdout."
+    )
+    parser.add_argument(
+        "--nover", action="store_true",
+        help="Set if accession number has no version number suffix, e.g., "
+             "AB123456 instead of AB123456.1"
+    )
+    parser.add_argument(
+        "--relabel", action="store_true",
+        help="Use metadata to replace sequence/tree labels with: "
+             "[organism]_[host]_[country]_[collection date]."
+    )
+    args = parser.parse_args()
+
+    assert '@' in args.email, "Use a valid e-mail address!"
+    assert args.delay >= 1.0, "Delay must be at least 1.0 seconds!"
+    assert args.batch > 0, "Batch size must be positive!"
+
+    fields = ('organism', 'mol_type', 'isolate', 'isolation_source', 'host',
+              'geo_loc_name', 'collection_date')
+    Entrez.email = args.email
+
+    # extract accessions from input
+    if args.format == "newick":
+        phy = Phylo.read(args.infile, "newick")
+        headers = [tip.name for tip in phy.get_terminals()]
+    else:
+        records = SeqIO.parse(args.infile, args.format)
+        headers = [record.description for record in records]
+
+    intermed = [find_accn(header, versioned=not args.nover) for header in headers]
+    accns = [a for a in intermed if a is not None]
+    if len(accns) == 0:
+        sys.stderr.write("\nERROR: Failed to parse any accession numbers from "
+                         f"{args.infile.name}!\n")
+        if args.infile.name.endswith(".nwk"):
+            sys.stderr.write("*** This seems to be a tree, did you forget to set `--format "
+                             "newick`? ***\n")
+        sys.exit()
+
+    sys.stderr.write(
+        f"Extracted {len(accns)} accession numbers from input file:\n")
+    sys.stderr.write(
+        f"  {', '.join(accns[:3])}, ..., {', '.join(accns[-3:])}\n")
+    if len(accns) < len(intermed):
+        sys.stderr.write(
+            f"Failed to parse accessions from {len(intermed)-len(accns)} "
+            f"records.\n")
+    sys.stderr.flush()
+
+    # prepare output file
+    if not args.relabel:
+        writer = DictWriter(args.outfile, fieldnames=('header',)+fields)
+        writer.writeheader()
+
+    # retrieve the metadata and write to file as CSV
+    labels = {}
+    for i in range(0, len(accns), args.batch):
+        query = ','.join(accns[i:(i+args.batch)])
+        rows = get_metadata(query, fields, db=args.db)
+        for j, row in enumerate(rows):
+            if args.relabel:
+                lab = f"{accns[i+j]}_{row['geo_loc_name']}_{row['collection_date']}"
+                labels.update({headers[i+j]: lab})
+            else:
+                row.update({"header": headers[i+j]})
+                writer.writerow(row)
+        time.sleep(args.delay)
+
+    if args.relabel:
+        if args.format == 'newick':
+            for tip in phy.get_terminals():
+                tip.name = labels[tip.name]
+            Phylo.write(phy, args.outfile, 'newick')
+        else:
+            # reload input file
+            records = SeqIO.parse(args.infile, args.format)
+            for record in records:
+                record.description = labels[record.description]
+                SeqIO.write(record, args.outfile, args.format)
+    
+    args.outfile.close()
---
+++ Bioplus/pair_align.py
@@ -0,0 +1,113 @@
+from io import StringIO
+from Bio import SeqIO
+import argparse
+import sys
+import tempfile
+import subprocess
+
+try:
+    from mpi4py import MPI
+    nprocs = MPI.COMM_WORLD.Get_size()
+    my_rank = MPI.COMM_WORLD.Get_rank()
+except ModuleNotFoundError:
+    sys.stderr.write("Running in serial mode...\n")
+    nprocs = 1
+    my_rank = 0
+
+description = """\
+Use MAFFT to align sequences to a reference sequence, discarding any 
+insertions.  This can also be used to trim sequences to a specific 
+region of the genome, such as a reference gene.
+
+To run in parallel, you must have mpi4py installed and call this script
+using mpirun, i.e.: 
+  mpirun -np <number of processes> python3 pair_align [...]
+The outputs will be written to separate files with integer suffixes.
+You can concatenate these outputs into a single FASTA file with a `cat` 
+command.
+"""
+
+
+def mafft(query, ref, binpath="mafft"):
+    """
+    :param query:  str, sequence to align to reference
+    :param ref:  str, reference sequence
+    :param binpath:  str, path to MAFFT executable file
+    :return:  dict
+    """
+    handle = tempfile.NamedTemporaryFile(delete=False)
+    s = f'>ref\n{ref}\n>query\n{query}\n'
+    handle.write(s.encode('utf-8'))
+    handle.close()
+
+    # call MAFFT on temporary file
+    stdout = subprocess.check_output([binpath, '--quiet', handle.name])
+    stdout = stdout.decode('utf-8')
+    result = list(SeqIO.parse(StringIO(stdout), "fasta"))
+    aref = str(result[0].seq)
+    aquery = str(result[1].seq)
+
+    newseq = ""
+    for i, aa in enumerate(aref):
+        if aa == '-':
+            continue
+        newseq += aquery[i]
+    return newseq
+
+
+if __name__ == "__main__":
+    # command-line interface
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "infile", type=argparse.FileType('r'),
+        help="input, file with unaligned sequences"
+    )
+    parser.add_argument(
+        "ref", type=argparse.FileType('r'),
+        help="input, file containing reference to align sequences to."
+    )
+    parser.add_argument(
+        "outfile", type=str,
+        help="output, path to write aligned FASTA; if running MPI, "
+             "script will append an integer suffix, e.g., '.1'"
+    )
+    parser.add_argument(
+        "-b", "--binpath", default="mafft",
+        help="Path to MAFFT executable file.  Defaults to 'mafft'."
+    )
+    parser.add_argument(
+        "-f", "--format", default="fasta",
+        help="format of sequence file format, passed to SeqIO.parse"
+    )
+    parser.add_argument(
+        "--verbose", action="store_true",
+        help="print progress monitoring messages to stderr"
+    )
+    args = parser.parse_args()
+
+    # read reference sequence
+    reference = SeqIO.read(args.ref, args.format)
+    ref = str(reference.seq)
+
+    records = SeqIO.parse(args.infile, args.format)
+    if nprocs == 1:
+        outfile = open(args.outfile, 'w')
+        for record in records:
+            if args.verbose:
+                sys.stderr.write(record.name+'\n')
+                sys.stderr.flush()
+            query = str(record.seq)
+            aligned = mafft(query=query, ref=ref, binpath=args.binpath)
+            outfile.write(f">{record.description}\n{aligned}\n")
+    elif nprocs > 1:
+        outfile = open(f"{args.outfile}.{my_rank}", 'w')
+        for rn, record in enumerate(records):
+            if rn % nprocs != my_rank:
+                continue
+            if args.verbose:
+                sys.stderr.write(f"({my_rank}/{nprocs}) running {record.name}\n")
+                sys.stderr.flush()
+            aligned = mafft(query=str(record.seq), ref=ref, binpath=args.binpath)
+            outfile.write(f">{record.description}\n{aligned}\n")
+
+    outfile.close()
